5178c8060263435fe79e26644e80cc221c662f06
Margo <m.yunanova@gmail.com>
2023-10-13 00:20:05 +0300

    implement User model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { WishesModule } from "./wishes/wishes.module";
+import { WishlistsModule } from "./wishlists/wishlists.module";
+import { OffersModule } from "./offers/offers.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/entities/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "127.0.0.1",
+      port: 5432,
+      username: "student",
+      password: "student",
+      database: "nest_project",
+      entities: [User],
+      schema: "nest_project",
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { IsNotEmpty, IsString, Length, IsEmail, IsUrl } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Length(2, 30)
+  @IsNotEmpty()
+  @IsString()
+  @Column({ unique: true })
+  usermame: string;
+
+  @Length(2, 200)
+  @IsString()
+  @Column({ default: "Пока ничего не рассказал о себе" })
+  about: string;
+
+  @IsUrl()
+  @Column({ default: "https://i.pravatar.cc/300" })
+  avatar: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Column({ unique: true })
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Column()
+  password: string;
+
+  @Column()
+  wishes: string;
+
+  @Column()
+  offers: string;
+
+  @Column()
+  wishlists: string;
+}
