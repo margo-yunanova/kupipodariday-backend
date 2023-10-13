50aa339595273783f6e423ca6ec10e87d5ee7f23
Margo <m.yunanova@gmail.com>
2023-10-13 19:16:01 +0300

    implement wishlist and offers models

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,21 @@
-export class Offer {}
+import { IsUrl } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Offer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user: string;
+
+  @IsUrl()
+  @Column()
+  item: string;
+
+  @Column({ type: "decimal", precision: 2 })
+  amount: number;
+
+  @Column()
+  hidden: false;
+}
--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,23 @@
-export class Wishlist {}
+import { IsUrl, Length } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Wishlist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Length(1, 250)
+  @Column()
+  name: string;
+
+  @Length(0, 1500)
+  @Column()
+  description: string;
+
+  @IsUrl()
+  @Column()
+  image: string;
+
+  @Column("simple-array")
+  items: string[];
+}
